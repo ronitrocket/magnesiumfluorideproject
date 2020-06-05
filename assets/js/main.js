f369def8662ee77aa02eb20df4900b15ddc501d7
RonitrocketYT <ronitrocket98@gmail.com>
2020-06-04 22:30:41 -0400

    added  how is it formed

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,9 +76,9 @@ NOTE: This is the custom jQuery file for the template
             element.typed({
                 strings: ["Formula: MgF" + subscript.sub(), "Magnesium has atomic number of 12, Fluorine is 9.", "The atomic mass of the compound is 43.", "The density of this compound is 3.15 g/cm3.", "Solubility is 0.012 g/100 mL", "Melting point is 1,263*C.", "Boiling point is 2,260*C.", "Solid at room temperature.", "Color is white.", "Not very lustrous, it looks only a little bit shiny.", "Has some level of reactivity."],
                 typeSpeed: 60,
-                backDelay: 500,
-                backSpeed: 5,
-                startDelay: 3000,
+                backDelay: 2000,
+                backSpeed: 1,
+                startDelay: 6000,
                 loop: true,
                 shuffle: false,
                 smartBackspace: true,
