73b1c00172bae11f0c688521c1c1d5ff8119af52
RonitrocketYT <ronitrocket98@gmail.com>
2020-06-04 21:38:06 -0400

    added basic facts

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,9 +71,10 @@ NOTE: This is the custom jQuery file for the template
    
         /*======== Intro typer ===========*/
         var element = $(".element");
+        var subscript = "2"
         $(function() {
             element.typed({
-                strings: ["VR/AR Enthusiast.", "AI/ML Enthusiast", "AI/ML Enthusiast", "Developer.", "Lifelong learner.", "Innovator at The Knowledge Society."],
+                strings: ["Formula: MgF" + subscript.sub(), "Magnesium has atomic number of 12, Fluorine is 9.", "The atomic mass of the compound is 43.", "The density of this compound is 3.15 g/cm3.", "Solubility is 0.012 g/100 mL", "Melting point is 1,263*C.", "Boiling point is 2,260*C.", "Solid at room temperature.", "Color is white.", "Not very lustrous, it looks only a little bit shiny.", "Has some level of reactivity."],
                 typeSpeed: 100,
                 loop: true,
             });
